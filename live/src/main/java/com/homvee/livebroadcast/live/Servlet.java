ac868f98e010b0f7abbe476c81ffa415823fa3d1
tanghongwei <Wert324tang>
2018-07-24 15:28:42 +0800

    xx

---
+++ live/src/main/java/com/homvee/livebroadcast/live/Servlet.java
@@ -0,0 +1,93 @@
+package com.homvee.livebroadcast.live;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.util.TextUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Copyright (c) 2018$. ddyunf.com all rights reserved
+ *
+ * @author Homvee.Tang(tanghongwei @ ddcloudf.com)
+ * @version V1.0
+ * @Description TODO(用一句话描述该文件做什么)
+ * @date 2018-07-24 15:13
+ */
+@WebServlet(name = "/xxxx" , urlPatterns = {"/get"})
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+
+        String result = doJsonPost("http://passport.douyu.com" , null);
+        response.getWriter().write(result);
+        response.getWriter().flush();
+        response.getWriter().close();
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        this.doPost(request , response);
+    }
+
+    public static String doJsonPost(String urlPath, String Json) {
+        // HttpClient 6.0被抛弃了
+        String result = "";
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlPath);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Charset", "UTF-8");
+            // 设置文件类型:
+            conn.setRequestProperty("Content-Type","text/html; charset=UTF-8");
+            // 设置接收类型否则返回415错误
+            //conn.setRequestProperty("accept","*/*")此处为暴力方法设置接受所有类型，以此来防范返回415;
+            // 往服务器里面发送数据
+            if (Json != null && !TextUtils.isEmpty(Json)) {
+                byte[] writebytes = Json.getBytes();
+                // 设置文件长度
+                conn.setRequestProperty("Content-Length", String.valueOf(writebytes.length));
+                OutputStream outwritestream = conn.getOutputStream();
+                outwritestream.write(Json.getBytes());
+                outwritestream.flush();
+                outwritestream.close();
+            }
+            if (conn.getResponseCode() == 200) {
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String tmp = reader.readLine();
+                while (StringUtils.isNotEmpty(tmp)){
+                    result = result + tmp;
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ live/src/main/webapp/load.jsp
@@ -0,0 +1,9 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ddyunf
+  Date: 2018/7/23
+  Time: 19:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
